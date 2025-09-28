71576cf4805123a43a1cf3bee393c2b67f015e75
Stamatis Karampogas <stamkbusiness@gmail.com>
2025-09-28 21:19:30 +0300

    Day6

---
+++ Day6/part2.py
@@ -0,0 +1,86 @@
+import re
+from enum import Enum
+
+TURN_ON = r"turn on (\d+),(\d+) through (\d+),(\d+)"
+TURN_OFF = r"turn off (\d+),(\d+) through (\d+),(\d+)"
+TOGGLE = r"toggle (\d+),(\d+) through (\d+),(\d+)"
+
+turn_on_pattern = re.compile(TURN_ON)
+turn_off_pattern = re.compile(TURN_OFF)
+toggle_pattern = re.compile(TOGGLE)
+
+grid = []
+brightness_grid =[]
+
+class State(Enum):
+    ON = 1
+    OFF = 2
+    TOGGLE = 3
+
+# x is for light turned off (for visualization 'cause I'm dumb)
+# y is for light turned on
+def create_initial_array():
+    grid = [["x" for column in range(1000)] for row in range(1000)]
+    return grid
+
+def create_brightness_grid():
+    brightness_grid = [[0 for column in range(1000)] for row in range(1000)]
+    return brightness_grid
+
+
+def update_grid(passed_grid: list, new_coordinates: tuple[int, int], passed_brightness: list, state: State) -> tuple[list, list]:
+    initial_place = new_coordinates[:2]
+    final_place = new_coordinates[-2:]
+
+    if state == state.ON:
+        for row in range(initial_place[0], final_place[0] + 1):
+            for column in range(initial_place[1], final_place[1] + 1):
+                passed_grid[row][column] = "y"
+                passed_brightness[row][column] +=1
+    if state == state.OFF:
+        for row in range(initial_place[0], final_place[0] + 1):
+            for column in range(initial_place[1], final_place[1] + 1):
+                if passed_brightness[row][column] == 0:
+                    continue
+                passed_brightness[row][column] -= 1
+    if state == state.TOGGLE:
+        for row in range(initial_place[0], final_place[0] + 1):
+            for column in range(initial_place[1], final_place[1] + 1):
+                    passed_brightness[row][column] += 2
+
+    return passed_grid, passed_brightness
+
+def find_lights_on(final_grid: list) -> int:
+    lights_on = 0
+    for row in range (0, 1000):
+        for column in range(0, 1000):
+            if final_grid[row][column] == "y":
+                lights_on += 1
+    return lights_on
+
+def find_brightness(final_brightness: list) -> int:
+    brightness = 0
+    for row in range (0, 1000):
+        for column in range(0, 1000):
+            brightness += final_brightness[row][column]
+    return brightness
+
+with open("input.txt") as file:
+    initial_grid = create_initial_array()
+    initial_brightness_grid = create_brightness_grid()
+    for i in file.readlines():
+        match_turn_on = turn_on_pattern.match(i)
+        match_turn_off = turn_off_pattern.match(i)
+        match_toggle = toggle_pattern.match(i)
+
+        if match_turn_on:
+            coords_on = tuple(map(int, match_turn_on.groups()))
+            initial_grid, initial_brightness_grid = update_grid(initial_grid, coords_on, initial_brightness_grid, state=State.ON)
+        if match_turn_off:
+            coords_off = tuple(map(int, match_turn_off.groups()))
+            initial_grid, initial_brightness_grid = update_grid(initial_grid, coords_off, initial_brightness_grid, state=State.OFF)
+        if match_toggle:
+            coords_toggle = tuple(map(int, match_toggle.groups()))
+            initial_grid, initial_brightness_grid = update_grid(initial_grid, coords_toggle, initial_brightness_grid, state=State.TOGGLE)
+    print(find_lights_on(initial_grid))
+    print(find_brightness(initial_brightness_grid))
